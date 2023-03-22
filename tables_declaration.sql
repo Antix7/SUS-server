fbcc2ff354f7db335f024be78bcae540c055f353
Antix7 <84929498+Antix7@users.noreply.github.com>
2023-03-22 22:17:39 +0100

    Changed table users to include email address

--- tables_declaration.sql
+++ tables_declaration.sql
@@ -65,12 +65,13 @@ CREATE TABLE sprzet
         FOREIGN KEY (wlasciciel_id) REFERENCES podmioty (podmiot_id)
 );
 
-CREATE TABLE users
+CREATE TABLE sus_database.users
 (
     username         VARCHAR(255)         NOT NULL
         PRIMARY KEY,
     password_hash    VARCHAR(255)         NOT NULL,
     czy_admin        TINYINT(1) DEFAULT 0 NOT NULL,
+    adres_email      VARCHAR(255)         NULL,
     data_wygasniecia DATE                 NULL
 );
 
